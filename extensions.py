cbe9bf4cf00793cb88829bd3d9971463c4bd1b9f
Natalia Galicka <natalia.galicka.programista@gmail.com>
2026-05-20 14:23:33 +0200

    Add instation of LoginManager

--- extensions.py
+++ extensions.py
@@ -1,7 +1,16 @@
 # Plik zawiera m.in. inicjalizację ORM - bazę danych, SQLAlchemy
 # Plik zawiera inizjalizację LoginManager oraz instancję CSRF
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 # Tworzę instancję bazy danych ORM
 db = SQLAlchemy()
+
+# Tworzę instancję LoginManager, do tworzenia stron logowania
+# Dzięki temu managerowi, nie będę msuiała sama definiować metod np. autoryzacja, zalogowanie użytkownika, czy jest zalogowany itd..
+# Ustawiam widok logowania, kategorię komunikatów, ustawienie komunikatu
+login_manager = LoginManager()
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
+login_manager.login_message = "Zaloguj się, aby uzyskać dostęp do tej strony"
 
