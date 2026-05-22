098e2793efc49a078950eea35e1c95587456633a
Natalia Galicka <natalia.galicka.programista@gmail.com>
2026-05-22 09:34:31 +0200

    Edit registration form - add first name and last name field. Remove username field. Remove username validation

--- forms.py
+++ forms.py
@@ -4,17 +4,12 @@ from wtforms.validators import DataRequired, Length, EqualTo, ValidationError, E
 from models import User
 
 class RegistrationForm(FlaskForm):
-    username = StringField("Nazwa użytkownika", validators=[DataRequired(), Length(min=4, max=150)])
+    first_name = StringField("Imię", validators=[DataRequired(), Length(max=150)])
+    last_name = StringField("Nazwisko", validators=[DataRequired(), Length(max=150)])
     email = StringField("Email", validators=[DataRequired(), Email(), Length(max=250)])
     password = PasswordField("Hasło", validators=[DataRequired(), Length(min = 6)])
     confirm_password = PasswordField("Powtórz hasło", validators=[DataRequired(), EqualTo('password', message = "Hasła muszą być takie same")])
     submit = SubmitField("Zarejestruj się")
-
-    # Walidacja użytkownika -> czy username jest unikalne
-    def validate_username(self, username):
-        existing_user = User.query.filter_by(username = username.data).first() #username.data = dane wprowadzone w formularzu
-        if existing_user:
-            raise ValidationError("Ta nazwa użytkownika jest już zajęta")
         
     # Walicja email -> czy email jest unikalny
     def validate_email(self, email):
