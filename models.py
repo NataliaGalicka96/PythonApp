2dd0d5abc9f47cb7b9cf15a352d2603e1ae69b46
Natalia Galicka <natalia.galicka.programista@gmail.com>
2026-05-22 09:31:52 +0200

    Add first_name and last_name to user model. Remove username from User model.

--- models.py
+++ models.py
@@ -10,7 +10,8 @@ from enums import UserRole
 # Zamiast pisać je ręcznie, dziedziczysz po UserMixin, który już je implementuje.
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(150), unique = True, nullable = False)
+    first_name = db.Column(db.String(150), nullable = False)
+    last_name = db.Column(db.String(150), nullable = False)
     password = db.Column(db.String(150), nullable = False)
     role = db.Column(db.Enum(UserRole), nullable = False, default = UserRole.JOB_SEEKER)
     created = db.Column(db.DateTime, default = datetime.now(UTC)) # UTC - uniwersalny światowy czas
