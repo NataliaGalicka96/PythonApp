fefd639406d0521f6b7ade61881ee9a54f1a08ad
Natalia Galicka <natalia.galicka.programista@gmail.com>
2026-05-25 13:20:28 +0200

    Add enums: JobType, Level, JobOfferType

--- enums.py
+++ enums.py
@@ -4,3 +4,29 @@ class UserRole(str, Enum):
     JOB_SEEKER = "job_seeker"
     RECRUITER = "recruiter"
     ADMIN = "admin"
+
+class JobOfferType(str, Enum):
+    B2B = "Kontrakt B2B"
+    DZIEŁO = "Umowa o dzieło"
+    UOP = "Umowa o pracę"
+    ZLECENIE = "Umowa zlecenie"
+    ZASTEPSTWO = "Umowa na zastępstwo"
+    PRAKTYKI = "Umowa o staż/praktyki"
+
+class Level(str, Enum):
+    PRAKTYKANT = "Praktykant"
+    JUNIOR = "Młodszy specjalista"
+    MID = "Specjalista"
+    SENIOR = "Starszy specjalista"
+    EKSPERT = "Ekspert"
+    KIEROWNIK = "Kierownik"
+    MANAGER = "Menedżer"
+    DYREKTOR = "Dyrektor"
+    PREZES = "Prezes"
+
+class JobType(str, Enum):
+    ZDALNA = "Zdalna"
+    HYBRYDOWA = "Hybrydowa"
+    STACJONARNA = "Stacjonarna"
+
+
