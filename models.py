b7765a87678e33b556d4c96907bc8f2e4f336b3e
Natalia Galicka <natalia.galicka.programista@gmail.com>
2026-05-26 06:21:49 +0200

    Create model of JobOffer table

--- models.py
+++ models.py
@@ -1,8 +1,8 @@
 # Modele klas - odzwierciedlenie tabel w bazie danych
 from extensions import db
 from flask_login import UserMixin
-from datetime import datetime, UTC
-from enums import UserRole
+from datetime import datetime, timedelta, UTC
+from enums import UserRole, JobOfferType, JobType, Level
 
 # UserMixin pochodzi z bilioteki flask_login
 # Flask-LOgin wymaga, żeby obiekt użytkownika miał określone metody: is_authenticated
@@ -18,3 +18,19 @@ class User(UserMixin, db.Model):
     created = db.Column(db.DateTime, default = datetime.now(UTC)) # UTC - uniwersalny światowy czas
 
 
+# Klasa reprezentująca ogłoszenie
+class JobOffer(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(500), nullable = False)
+    description = db.Column(db.String(), nullable = False)
+    responsibilites = db.Column(db.String(), nullable = False)
+    location = db.Column(db.String(255), nullable = False)
+    salary_min = db.Column(db.Float(), nullable = True)
+    salary_max = db.Column(db.Float(), nullable = True)
+    type_of_contract = db.Column(db.Enum(JobOfferType), nullable = False, default = JobOfferType.UOP)
+    level = db.Column(db.Enum(Level), nullable = False)
+    job_type = db.Column(db.Enum(JobType), nullable = False)
+    is_active = db.Column(db.Boolean, nullable = False, default = True)
+    created = db.Column(db.DateTime, default = datetime.now(UTC)) # UTC - uniwersalny światowy czas
+    expired = db.Column(db.DateTime, nullable = False, default = datetime.now(UTC) + timedelta(days=30))
+    
