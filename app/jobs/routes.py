cfe3f60608f153b662d79cd835ea3fb30d5ae9ea
Natalia Galicka <natalia.galicka.programista@gmail.com>
2026-06-01 15:57:28 +0200

    Add method to save/unsave job offer

--- app/jobs/routes.py
+++ app/jobs/routes.py
@@ -1,12 +1,19 @@
 from flask import (
     Blueprint,
     render_template,
-    request
+    request,
+    jsonify
 )
 
+from flask_login import login_required, current_user
+
 from app.extensions import db
-from app.models.job_offer import JobOffer
+from app.models.job_offer import JobOffer, SavedJobOffer
 from app.jobs.forms import JobSearchForm
+from app.__init__ import create_app
+import logging
+
+
 
 jobs_bp = Blueprint(
     "jobs",
@@ -98,3 +105,52 @@ def job_details(job_id):
     offer = JobOffer.query.get_or_404(job_id)
 
     return render_template("jobs/job_details.html", offer=offer)
+
+
+# ZAPISANIE OFERTY DO ULUBIONYCH
+@jobs_bp.route("/jobs/<int:id>/save", methods = ["POST"])
+@login_required # Tylko zalogowany użytkownik
+def save_job(id):
+    # Pobranie oferty o tym id
+    job = JobOffer.query.get_or_404(id)
+
+
+    # Zasada - jeśli jest już w ulubionych dla tego użytkownika, to usuniemy z ulubionych, a jeśli nei jest to dodamy
+    existing_save = SavedJobOffer.query.filter_by(
+        user_id = current_user.id,
+        job_offer_id = job.id
+    ).first()
+
+
+    try:
+        if existing_save:
+            db.session.delete(existing_save) #Usuwam wpis z tabeli
+            db.session.commit()
+            return jsonify({
+                "saved": False,
+                "message": "Ta oferta została usunięta z ulubionych"
+            }), 201
+        
+         # Zapisywanie do ulubionych
+        saved_job = SavedJobOffer (
+            user_id = current_user.id,
+            job_offer_id = job.id
+        )
+
+        db.session.add(saved_job)
+        db.session.commit()
+
+        return jsonify({
+            "saved": True,
+            "message": "Dodano ofertę do ulubionych"
+        }), 201
+    except Exception as e:
+            db.session.rollback()
+            create_app.logger.error(f"Wystąpił błąd podczas")
+            return jsonify({
+                "error", "Wystąpił błąd podczas dodawania oferty do ulubionych"
+            }), 500
+    
+    
+
+   
