6ebae915bc1cf80ea66d0c389d740a663bac69f7
Natalia Galicka <natalia.galicka.programista@gmail.com>
2026-06-01 07:02:39 +0200

    Add comments to method which search offers by localization or company name or title

--- app/jobs/routes.py
+++ app/jobs/routes.py
@@ -33,17 +33,28 @@ def home():
 @jobs_bp.route("/jobs")
 def jobs():
 
+    # Tworzenie formularza i przekazanie danych z URL np. /jobs?query=python&location=warszawa request.args zawiera 
+    #    {
+    #    "query": "python",
+    #   "location": "warszawa"
+    # }
+    # czyli np. form.query.data = "python"
     form = JobSearchForm(request.args)
 
+    # Wyszukuje w bazie danych aktywnych ogłoszeń
     jobs_query = JobOffer.query.filter_by(
         is_active=True
     )
 
     # wyszukiwarka
+    # Sprawdzenie, czy uzytkownik wpisał coś w wyszukiwarkę
     if form.query.data:
 
+        # Tworzę to co wyszukujemy, czyli np: %python%
         search = f"%{form.query.data}%"
 
+        # znajdź wszystko, co zawiera „python”
+        # case insensitive LIKE - czyli PYTHON, python, Python -> wszystko pasuje, nie zwraca uwagi na wielkość liter
         jobs_query = jobs_query.filter(
             db.or_(
                 JobOffer.title.ilike(search),
@@ -52,17 +63,24 @@ def jobs():
         )
 
     # lokalizacja
+    # Sprawdzamy, czy użytkownik wpisał lokalizację
     if form.location.data:
 
+        #np. %Warszawa%
         location = f"%{form.location.data}%"
 
+
+        # Wyszukuję w bazie danych wszystkie oferty, które w lokalizacji mają Warszawa -> bez znaczenia wielkość liter
         jobs_query = jobs_query.filter(
             JobOffer.location.ilike(location)
         )
 
-
+    # PAGINACJA
+    # Czytasz numer strony z URL np. /jobs?page=3 -> wtedy page = 3, jeśli nie ma to domyślnie page = 1
     page = request.args.get("page", 1, type=int)
 
+    # Sortowanie ofert w bazie danych malejącowo wg daty utworzenia
+    # Wyświetlanie na stronie po 10 ogłoszeń
     offers = (
     jobs_query
     .order_by(JobOffer.created.desc())
