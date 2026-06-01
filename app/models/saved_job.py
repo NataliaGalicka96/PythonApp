32af05c0be5c7b50b060b25e29d31f4dcb8099e9
Natalia Galicka <natalia.galicka.programista@gmail.com>
2026-06-01 15:33:29 +0200

    Add saved offers model

---
+++ app/models/saved_job.py
@@ -0,0 +1,36 @@
+# id
+# user_id
+# job_offer_id
+# created_at
+from datetime import datetime, UTC
+from app.extensions import db
+
+class SavedJobOffer(db.Model):
+    __tablename__ = "saved_job_offer"
+
+    id = db.Column(
+        db.Integer,
+        primary_key = True
+    )
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey(
+            "user.id",
+            name = "fk_job_offer_user_liked"
+        ),
+        nullable = False
+    )
+
+    job_offer_id = db.Column(
+        db.Integer,
+        db.ForeignKey(
+            "job_offer.id",
+            name = "fk_job_offer_liked"
+        )
+    )
+
+    created_at = db.Column(
+        db.DateTime,
+        default = datetime.now(UTC)
+    )
