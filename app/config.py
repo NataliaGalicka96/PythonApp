95c5c9b87a9ed34c8ff70850b202d72131b0b634
Natalia Galicka <natalia.galicka.programista@gmail.com>
2026-06-11 09:07:32 +0200

    Add REMEMBER_COOKIE_DURATION to config file

--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from datetime import timedelta
 
 # pip install python-dotenv
 # Ładowanie zmiennych z pliku .env
@@ -21,3 +22,6 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     MAX_CONTENT_LENGTH = 5 * 1024 * 1024
+
+    # Dodanie konfiguracji dla opcji "Zapamiętaj mnie - przy logowaniu", żeby opcja działała np. 30 dni a nie domyślnie cały czas
+    REMEMBER_COOKIE_DURATION = timedelta(days=30)
